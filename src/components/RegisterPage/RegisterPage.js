17b81b0a371d192529628a7a71578aa3180fb281
yonghyun <dydgus2433@naver.com>
2021-02-01 08:04:54 +0900

    에러 찾느라 개고생

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <form>
+      <label>Example</label>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input name="example" defaultValue="test" />
+
+      <label>exampleRequired</label>
+      {/* include validation with required or other standard HTML validation rules */}
+      <input name="exampleRequired" />
+      {/* errors will return when field validation fails  */}
+
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default RegisterPage;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import Reducer from "./redux/reducer";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
-import Reducer from "./redux/reducers";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const createStoreWithMiddleware = applyMiddleware(
   promiseMiddleware,
   ReduxThunk
-);
+)(createStore);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,13 +26,15 @@ ReactDOM.render(
           window.__REDUX_DEVTOOLS_EXTENSION__()
       )}
     >
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
