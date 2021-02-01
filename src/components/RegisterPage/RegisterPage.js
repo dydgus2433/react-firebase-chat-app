25963e480fe56f2841ba34fa1ce0892605f602b1
yonghyun <dydgus2433@naver.com>
2021-02-02 08:02:58 +0900

    register page validate check

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,19 +1,70 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 function RegisterPage() {
+  const { register, watch, errors } = useForm();
+  const password = useRef();
+  password.current = watch("password");
+  console.log(watch("email"));
+  console.log("passowrd.current", password.current);
+
   return (
-    <form>
-      <label>Example</label>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input name="example" defaultValue="test" />
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      <form>
+        <label>Email</label>
+        <input
+          name="email"
+          type="email"
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>This field is required</p>}
+        <label>Name</label>
+        <input name="name" ref={register({ required: true, maxLength: 10 })} />
+        {errors.name && errors.name.type === "required" && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>Your input exceed maximum length</p>
+        )}
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          ref={register({ required: true, minLength: 10 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>Password must have at least 6 characters</p>
+        )}
+        <label>Password Confirm</label>
+        <input
+          name="password_confirm"
+          type="password"
+          ref={register({
+            required: true,
+            validate: (value) => value === password.current,
+          })}
+        />
+        {errors.password_confirm &&
+          errors.password_confirm.type === "required" && (
+            <p>This password confirm field is required</p>
+          )}
+        {errors.password_confirm &&
+          errors.password_confirm.type === "validate" && (
+            <p>This password do not match</p>
+          )}
 
-      <label>exampleRequired</label>
-      {/* include validation with required or other standard HTML validation rules */}
-      <input name="exampleRequired" />
-      {/* errors will return when field validation fails  */}
-
-      <input type="submit" />
-    </form>
+        <input type="submit" />
+        <Link to="/login" style={{ color: "gray", textDecoration: "none" }}>
+          이미 아이디가 있다면...
+        </Link>
+      </form>
+    </div>
   );
 }
 
